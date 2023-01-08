a2a2f213e0edfd7283fc1d0e106a554d0446da30
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-08 22:03:31 +0700

    update CardPengecekan shadow

--- views/driver/proses-pengecekan/CardPengecekan.js
+++ views/driver/proses-pengecekan/CardPengecekan.js
@@ -115,7 +115,7 @@ const CardPengecekan = () => {
 
   return (
     <div className="w-full px-4">
-      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-sm rounded">
         <CardHeader
           title="Proses Pengecekan"
           actionButton={
