11ed5a00de95ce50ef4bdb3105b50cad9df5fedd
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-08 22:39:37 +0700

    add leader views

--- pages/leader/riwayat-pengecekan.js
+++ pages/leader/riwayat-pengecekan.js
@@ -1,7 +1,14 @@
+import FilterRiwayat from 'views/leader/riwayat-pengecekan/FilterRiwayat';
+import CardRiwayat from 'views/leader/riwayat-pengecekan/CardRiwayat';
 import Leader from 'layouts/Leader';
 
 const RiwayatPengecekan = () => {
-  return null;
+  return (
+    <div className="flex flex-wrap">
+      <FilterRiwayat />
+      <CardRiwayat />
+    </div>
+  );
 };
 
 RiwayatPengecekan.layout = Leader;
--- pages/leader/riwayat-perbaikan.js
+++ pages/leader/riwayat-perbaikan.js
@@ -1,7 +1,14 @@
+import FilterRiwayat from 'views/leader/riwayat-perbaikan/FilterRiwayat';
+import CardRiwayat from 'views/leader/riwayat-perbaikan/CardRiwayat';
 import Leader from 'layouts/Leader';
 
 const RiwayatPerbaikan = () => {
-  return null;
+  return (
+    <div className="flex flex-wrap">
+      <FilterRiwayat />
+      <CardRiwayat />
+    </div>
+  );
 };
 
 RiwayatPerbaikan.layout = Leader;
