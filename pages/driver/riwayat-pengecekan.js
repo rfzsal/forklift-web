c79dc7ba0293d973a6e9a76dcd9cf02fe9d4d3dc
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-08 22:18:06 +0700

    add riwayat-pengecekan view

--- pages/driver/riwayat-pengecekan.js
+++ pages/driver/riwayat-pengecekan.js
@@ -1,7 +1,14 @@
+import CardRiwayat from 'views/driver/riwayat-pengecekan/CardRiwayat';
+import FilterRiwayat from 'views/driver/riwayat-pengecekan/FilterRiwayat';
 import Driver from 'layouts/Driver';
 
 const RiwayatPengecekan = () => {
-  return null;
+  return (
+    <div className="flex flex-wrap">
+      <FilterRiwayat />
+      <CardRiwayat />
+    </div>
+  );
 };
 
 RiwayatPengecekan.layout = Driver;
