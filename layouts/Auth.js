07c3c45f5fa529ef054e91981a4dc57bcda37198
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-08 23:07:11 +0700

    update auth bg

--- layouts/Auth.js
+++ layouts/Auth.js
@@ -4,7 +4,7 @@ const Auth = ({ children }) => {
       <main>
         <section className="relative w-full h-full py-40 min-h-screen">
           <div
-            className="absolute top-0 w-full h-full bg-lightBlue-600 bg-no-repeat bg-full"
+            className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
             style={{
               backgroundImage: "url('/img/register_bg_2.png')",
             }}
