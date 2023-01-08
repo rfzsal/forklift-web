1c7896067264a6e28d69fdfea41fdf9d9e6b4b50
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-08 22:22:21 +0700

    add riwayat-perbaikan view

--- pages/driver/riwayat-perbaikan.js
+++ pages/driver/riwayat-perbaikan.js
@@ -1,7 +1,14 @@
+import CardRiwayat from 'views/driver/riwayat-perbaikan/CardRiwayat';
+import FilterRiwayat from 'views/driver/riwayat-perbaikan/FilterRiwayat';
 import Driver from 'layouts/Driver';
 
 const RiwayatPerbaikan = () => {
-  return null;
+  return (
+    <div className="flex flex-wrap">
+      <FilterRiwayat />
+      <CardRiwayat />
+    </div>
+  );
 };
 
 RiwayatPerbaikan.layout = Driver;
