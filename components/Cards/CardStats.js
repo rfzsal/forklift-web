54e59029c7fbec5cec611ec409ee63aa15aa29bc
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-18 12:15:21 +0700

    update dashboard header

--- components/Cards/CardStats.js
+++ components/Cards/CardStats.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-export default function CardStats({
+const CardStats = ({
   statSubtitle,
   statTitle,
-  statArrow,
-  statPercent,
-  statPercentColor,
-  statDescripiron,
   statIconName,
   statIconColor,
-}) {
+}) => {
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
@@ -35,49 +28,10 @@ export default function CardStats({
               </div>
             </div>
           </div>
-          <p className="text-sm text-blueGray-400 mt-4">
-            <span className={statPercentColor + ' mr-2'}>
-              <i
-                className={
-                  statArrow === 'up'
-                    ? 'fas fa-arrow-up'
-                    : statArrow === 'down'
-                    ? 'fas fa-arrow-down'
-                    : ''
-                }
-              ></i>{' '}
-              {statPercent}%
-            </span>
-            <span className="whitespace-nowrap">{statDescripiron}</span>
-          </p>
         </div>
       </div>
     </>
   );
-}
-
-CardStats.defaultProps = {
-  statSubtitle: 'Traffic',
-  statTitle: '350,897',
-  statArrow: 'up',
-  statPercent: '3.48',
-  statPercentColor: 'text-emerald-500',
-  statDescripiron: 'Since last month',
-  statIconName: 'far fa-chart-bar',
-  statIconColor: 'bg-red-500',
 };
 
-CardStats.propTypes = {
-  statSubtitle: PropTypes.string,
-  statTitle: PropTypes.string,
-  statArrow: PropTypes.oneOf(['up', 'down']),
-  statPercent: PropTypes.string,
-  // can be any of the text color utilities
-  // from tailwindcss
-  statPercentColor: PropTypes.string,
-  statDescripiron: PropTypes.string,
-  statIconName: PropTypes.string,
-  // can be any of the background color utilities
-  // from tailwindcss
-  statIconColor: PropTypes.string,
-};
+export default CardStats;
--- components/Headers/DashboardHeader.js
+++ components/Headers/DashboardHeader.js
@@ -6,50 +6,26 @@ const DashboardHeader = ({ children }) => {
       <div className="relative bg-blueGray-800 md:pt-32 pb-32 pt-12">
         <div className="px-4 md:px-10 mx-auto w-full">
           <div className="flex flex-wrap mb-8">
-            <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+            <div className="w-full lg:w-6/12 xl:w-4/12 px-4">
               <CardStats
                 statSubtitle="TOTAL PENGECEKAN"
                 statTitle="27"
-                statArrow="up"
-                statPercent="3.48"
-                statPercentColor="text-emerald-500"
-                statDescripiron="Sejak kemarin"
                 statIconName="fas fa-search"
                 statIconColor="bg-orange-500"
               />
             </div>
-            <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+            <div className="w-full lg:w-6/12 xl:w-4/12 px-4">
               <CardStats
                 statSubtitle="TOTAL PERBAIKAN"
                 statTitle="19"
-                statArrow="down"
-                statPercent="3.48"
-                statPercentColor="text-red-500"
-                statDescripiron="Sejak kemarin"
                 statIconName="fas fa-cogs"
                 statIconColor="bg-lightBlue-500"
               />
             </div>
-            <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-              <CardStats
-                statSubtitle="BELUM DIPERBAIKI"
-                statTitle="10"
-                statArrow="down"
-                statPercent="1.10"
-                statPercentColor="text-orange-500"
-                statDescripiron="Sejak kemarin"
-                statIconName="fas fa-wrench"
-                statIconColor="bg-red-500"
-              />
-            </div>
-            <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+            <div className="w-full lg:w-6/12 xl:w-4/12 px-4">
               <CardStats
                 statSubtitle="SUDAH DIPERBAIKI"
                 statTitle="9"
-                statArrow="up"
-                statPercent="12"
-                statPercentColor="text-emerald-500"
-                statDescripiron="Sejak kemarin"
                 statIconName="fas fa-check"
                 statIconColor="bg-emerald-500"
               />
