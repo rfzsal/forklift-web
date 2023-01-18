382f89e7142c726594295a9a1a3fa5be17e6f2e1
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-18 13:43:50 +0700

    update riwayat hook

--- hooks/usePengecekan.js
+++ hooks/usePengecekan.js
@@ -43,7 +43,7 @@ const useProvidePengecekan = () => {
     return () => clearInterval(loop);
   }, []);
 
-  return { riwayat };
+  return { riwayat, refresh };
 };
 
 export { ProvidePengecekan, usePengecekan };
