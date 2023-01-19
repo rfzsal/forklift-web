d6e0f18ecbe413169afe80915bd6d2d623e04e16
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-19 10:09:44 +0700

    update user dropdown

--- components/Dropdowns/UserDropdown.js
+++ components/Dropdowns/UserDropdown.js
@@ -1,5 +1,6 @@
 import { useState, createRef } from 'react';
 import { createPopper } from '@popperjs/core';
+import Link from 'next/link';
 
 const UserDropdown = ({ avatarSeed }) => {
   const [dropdownPopoverShow, setDropdownPopoverShow] = useState(false);
@@ -45,43 +46,16 @@ const UserDropdown = ({ avatarSeed }) => {
           'bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48'
         }
       >
-        <a
-          href="#pablo"
-          className={
-            'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Action
-        </a>
-        <a
-          href="#pablo"
-          className={
-            'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Another action
-        </a>
-        <a
-          href="#pablo"
-          className={
-            'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Something else here
-        </a>
-        <div className="h-0 my-2 border border-solid border-blueGray-100" />
-        <a
-          href="#pablo"
-          className={
-            'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Seprated link
-        </a>
+        <Link href="/auth/keluar">
+          <a
+            href="/auth/keluar"
+            className={
+              'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
+            }
+          >
+            Keluar
+          </a>
+        </Link>
       </div>
     </>
   );
