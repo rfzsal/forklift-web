d21cd3da4118cbb801ed9dcaadb795bd1e8865e9
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-21 15:25:42 +0700

    print button

--- pages/print/riwayat-perbaikan.js
+++ pages/print/riwayat-perbaikan.js
@@ -80,6 +80,16 @@ const RiwayatPerbaikan = () => {
 
   return (
     <div className="container flex flex-wrap mx-auto">
+      <div className="p-4 mx-4 no-print">
+        <button
+          className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-semibold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => window.print()}
+        >
+          Print
+        </button>
+      </div>
+
       <CardDetail
         date={createDate()}
         dataDriver={createDataDriver()}
