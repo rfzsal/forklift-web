bd51ec92a3d3226975372b80f140c67431065a5a
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-23 12:34:09 +0700

    update backdoor

--- pages/api/backdoor/register.js
+++ pages/api/backdoor/register.js
@@ -3,14 +3,14 @@ const { hash } = require('bcrypt');
 const { query } = require('lib/server/mysql');
 
 const handler = async (req, res) => {
-  const { username, role } = req.query;
-  if (!username || !role) return res.status(400).end();
+  const { username, role, password } = req.query;
+  if (!username || !role || !password) return res.status(400).end();
 
-  const id = `${Math.floor(100000 + Math.random() * 900000)}`;
-  const password = await hash(username, 10);
+  const newId = `${Math.floor(100000 + Math.random() * 900000)}`;
+  const newPassword = await hash(password, 10);
 
   const sql = 'INSERT INTO data_pengguna VALUES (?, ?, ?, ?)';
-  const values = [id, username, password, role];
+  const values = [newId, username, newPassword, role];
 
   const [error, rows] = await query(sql, values);
   res.status(200).send({ error, data: rows });
