ee38136caf6ac30b48d1dfc36c01ae1be7cdac5a
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-23 11:17:46 +0700

    update backdoor

--- pages/api/backdoor/register.js
+++ pages/api/backdoor/register.js
@@ -7,7 +7,7 @@ const handler = async (req, res) => {
   if (!username || !role) return res.status(400).end();
 
   const id = `${Math.floor(100000 + Math.random() * 900000)}`;
-  const password = await hash('24agustus', 10);
+  const password = await hash(username, 10);
 
   const sql = 'INSERT INTO data_pengguna VALUES (?, ?, ?, ?)';
   const values = [id, username, password, role];
