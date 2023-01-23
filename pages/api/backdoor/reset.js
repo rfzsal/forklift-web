8bfbc1e6dfc164086fc9392f80078ab8dbf3e915
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-23 12:50:36 +0700

    update backdoor

--- pages/api/backdoor/reset.js
+++ pages/api/backdoor/reset.js
@@ -1,10 +1,10 @@
 const { query } = require('lib/server/mysql');
 
 const handler = async (req, res) => {
-  const sql = 'DELETE FROM data_pengecekan WHERE 1=1';
+  const [error1] = await query('DELETE FROM data_pengguna WHERE 1=1', null);
+  const [error2] = await query('DELETE FROM data_pengecekan WHERE 1=1', null);
 
-  const [error, rows] = await query(sql, null);
-  res.status(200).send({ error, data: rows });
+  res.status(200).send({ error: [error1, error2], data: 'Ok' });
 };
 
 export default handler;
